c8bd7f7f8746da60d2e59c230d75ffe9e2b11b02
MailMan108 <82180111+MailMan108@users.noreply.github.com>
2021-04-08 14:45:07 -0600

    Update index.js

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import './fonts/azukiB.ttf';
+import './font/azukiB.ttf';
 
 ReactDOM.render(
   <React.StrictMode>
